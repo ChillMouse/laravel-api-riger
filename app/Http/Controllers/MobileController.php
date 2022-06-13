30ef83442885dac8ebb90c565b5977727eaaabd7
ChillMouse <koftun251201@inbox.ru>
2022-06-14 00:30:19 +0700

    develop: валидация уникального пользователя

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -4,19 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class MobileController extends Controller
 {
     public function register(Request $request) {
         if ($login = $request->input('login') and $password = $request->input('password')) {
+            $input = ['email' => $login];
+            $rules = [
+                'email' => 'unique:users'
+            ];
+            $validate = Validator::make($input, $rules);
 
-            $user = new User();
+            if ($validate->passes()) {
+                $user = new User();
 
-            $user->email = $login;
-            $user->password = $password;
+                $user->email = $login;
+                $user->password = $password;
 
-            $user->save();
-            $answer = ['status' => 'success', 'text' => 'Успешно зарегистрирован'];
+                $user->save();
+                $answer = ['status' => 'success', 'text' => 'Успешно зарегистрирован'];
+            } else {
+                $answer = ['status' => 'error', 'text' => 'Такой пользователь уже существует'];
+            }
+
         } else {
             $answer = ['status' => 'error', 'text' => 'Не указан логин или пароль'];
         }
