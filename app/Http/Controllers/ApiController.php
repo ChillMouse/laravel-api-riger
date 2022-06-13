555f6f0302ba371f3f2f4d202b985158fc6fbf18
ChillMouse <koftun251201@inbox.ru>
2022-06-14 00:38:35 +0700

    develop: добавлена обработка уникальности логина, bug fix

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -15,7 +15,7 @@ class ApiController extends Controller
     public function register(Request $request) {
         $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'email' => 'required',
+            'email' => 'required|unique:users,email',
             'password' => 'required',
             'age' => 'required',
             'sex' => 'required',
