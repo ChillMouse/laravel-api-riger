1a15999db29dea6384a82c2032560ca60d43c2fc
ChillMouse <rorkopxrnrxv@mail.ru>
2022-10-16 23:18:19 +0700

    develop: controller update, observe doctors

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Appointments;
 use App\Models\Images;
 use App\Models\Messages;
 use App\Models\User;
@@ -14,5 +15,10 @@ class FeedController extends Controller
         $messages = Messages::all();
         $images = Images::all();
         return view('tables', compact('users', 'messages', 'images'));
+    }
+
+    public function indexDoctors() {
+        $appointments = Appointments::all()->toArray();
+        return view('table_doctor', compact('appointments'));
     }
 }
