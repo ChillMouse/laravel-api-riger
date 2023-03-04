feb9cad130a76e8b692a8adad93a27fe36403d84
ChillMouse <rorkopxrnrxv@mail.ru>
2023-03-05 01:26:16 +0700

    develop: del referer

--- app/Http/Middleware/AuthByHeaders.php
+++ app/Http/Middleware/AuthByHeaders.php
@@ -25,9 +25,9 @@ class AuthByHeaders
             return redirect('api/error_auth');
         }
 
-        if ($request->header('Referer') != 'api.uzkanova.ru/api') {
-            return redirect('api/error_auth');
-        }
+//        if ($request->header('Referer') != 'api.uzkanova.ru/api') {
+//            return redirect('api/error_auth');
+//        }
 
         if ($request->header('Accept') != 'application/json') {
             return redirect('api/error_auth');
